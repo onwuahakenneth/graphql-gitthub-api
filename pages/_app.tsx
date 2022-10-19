8887993c4098e3c07a51168dd7948ac76eb44127
onwuahakenneth <onwuahakenneth@gmail.com>
2022-10-19 10:24:30 +0100

    add pagination and filtering features

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Layout } from "../src/components/layout";
+import { IssuesContextProvider } from "../src/context/issuesRepo.context";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <IssuesContextProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </IssuesContextProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,86 +1,123 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage, GetStaticProps } from "next";
+import { useContext, useEffect, useState } from "react";
+import Head from "next/head";
+import {
+  ApolloClient,
+  InMemoryCache,
+  gql,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { IssueInterface } from "../src/types";
+import { IssueComponent } from "../src/components/issue";
+import { Pagination } from "../src/components/pagination";
+import IssuesContext from "../src/context/issuesRepo.context";
 
-const Home: NextPage = () => {
+interface Props {
+  fetchedIssues: IssueInterface[];
+}
+const Home: NextPage<Props> = ({ fetchedIssues }) => {
+  const { setIssuesData, currentIssues, setCurrentIssues, setFilteredIssues, itemsPerPage, filteredIssues} = useContext(IssuesContext);
+
+  useEffect(() => {
+    setIssuesData(fetchedIssues);
+    setFilteredIssues(fetchedIssues.slice(0, itemsPerPage))
+    setCurrentIssues(fetchedIssues.slice(0, itemsPerPage))
+  }, [fetchedIssues]);
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="flex flex-col items-center">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+      <main className="bg-gray-200 w-full lg:max-w-6xl h-screen pt-20 pb-16 px-4 auto overflow-scroll flex flex-col items-center">
+        {
+          filteredIssues.length < 0  && <p className="font-bold text-gray-600 mt-10">No Issues Found</p>
+        }
+        {currentIssues.length > 0 ? (
+          <>
+            <div className="mt-2 mb-6">
+              {filteredIssues.map((issue) => (
+                <IssueComponent key={issue.id} issue={issue} />
+              ))}
+            </div>
+            <Pagination itemsPerPage={10} />
+          </>
+        ) : (
+          <p className="mx-auto mt-10 h-8 w-8 border-gray-500 border-l-gray-300 border-x-4 border-y-4 rounded-full duration-800 ease-in-out animate-spin"></p>
+        )}
       </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
+
+export const getStaticProps: GetStaticProps = async () => {
+  const httpLink = createHttpLink({
+    uri: process.env.GITHUB_GRAPHQL_ENDPOINT,
+  });
+
+  const authLink = setContext((_, { headers }) => {
+    return {
+      headers: {
+        ...headers,
+
+        authorization: `Bearer ${process.env.GITHUB_ACCESS_TOKEN}`,
+      },
+    };
+  });
+
+  const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+
+    cache: new InMemoryCache(),
+  });
+
+  const { data } = await client.query({
+    query: gql`
+      query {
+        repository(name: "reactjs.org", owner: "reactjs") {
+          issues(last: 50) {
+            edges {
+              node {
+                id
+                state
+                # body
+                # bodyText
+                title
+                createdAt
+                author {
+                  login
+                }
+                bodyText
+                closedAt
+                comments(first: 5) {
+                  edges {
+                    node {
+                      id
+                      bodyText
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    `,
+  });
+
+  const { issues } = data.repository;
+
+  const issuesData: IssueInterface[] = issues.edges.map(
+    (issue: any) => issue.node
+  );
+
+  return {
+    props: {
+      fetchedIssues: issuesData,
+    },
+  };
+};
